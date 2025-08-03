9097664604bd25bba2979246fbd696cde9632383
Sanjan A P <sanjanap.cy21@sahyadri.edu.in>
2025-08-03 21:21:17 +0530

    first commit

---
+++ src/main/java/com/zephyros/urbanup/UrbanupApplication.java
@@ -0,0 +1,13 @@
+package com.zephyros.urbanup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanupApplication.class, args);
+    }
+
+}
